d400f45d51661727eea6f87f612c46d4f6f36573
guillermosolis727 <guillermosolis727@gmail.com>
2016-01-21 02:52:20 +0000

    Clase 1

---
+++ taxi/db/migrate/20160121023329_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :string
+      t.string :email
+      t.string :birthday
+      t.string :date
+      t.boolean :admin
+
+      t.timestamps null: false
+    end
+  end
+end
